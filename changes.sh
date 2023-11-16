8764640c64d8ee40d0a8854506b725a0545efeac
byron7 <byron7@users.noreply.github.com>
2023-11-15 22:56:00 -0600

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -1,20 +1,20 @@
 #! /usr/bin/env bash
 LOGIN = lastlog | grep $USER | awk '{print $2}'
 
-if [ $LOGIN =~ "Never" ]; then
-	read -p "Desea cambiar el hostname y la IP del servidor [s/n] ahora: " RESPUESTA
+if [[ $LOGIN =~ "Never" ]]; then
+	read -p "Desea cambiar el hostname y la IP del servidor ahora [s/n]: " RESPUESTA
 
-	if [ $RESPUESTA = "n" ]; then
+	if [[ $RESPUESTA = "s" ]]; then
 		read -p "Ingrese el hostname al que se quiere cambiar: " NEW_HOSTNAME
-		read -p "Escriba la IP que desea asignarle como por ejemplo 192.168.1.1/24: " IP_ADDRESS
+		read -p "Escriba la IP que desea asignarle como por ejemplo 192.168.1.1: " IP_ADDRESS
 		read -p "La ip del gateway: " GATEWAY_ADDRESS
 		read -p "La IP del DNS: " PRIMARY_DNS_ADDRESS
 
 		sudo hostnamectl set-hostname "$NEW_HOSTNAME"
 
-		mv /etc/netplan/99-custom.yaml /etc/netplan/99-custom.yaml.$(date +%Y%m%d)
+		sudo mv /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.$(date +%Y%m%d)
 
-		cat > /etc/netplan/99-custom.yaml << "EOF"
+		cat > /etc/netplan/00-installer-config.yaml << "EOF"
 		network:
 		  ethernets:
 		    ens33:
