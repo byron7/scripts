6a258fe72ec2f32aa03b48ab9ad996b19f9c3e33
byron7 <byron7@users.noreply.github.com>
2023-11-16 00:09:01 -0600

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -6,29 +6,9 @@ if [[ $LOGIN =~ "Never" ]]; then
 
 	if [[ $RESPUESTA = "s" ]]; then
 		read -p "Ingrese el hostname al que se quiere cambiar: " NEW_HOSTNAME
-		read -p "Escriba la IP que desea asignarle como por ejemplo 192.168.1.1: " IP_ADDRESS
-		read -p "La ip del gateway: " GATEWAY_ADDRESS
-		read -p "La IP del DNS: " PRIMARY_DNS_ADDRESS
+		read -p "Escriba la IP que desea asignarle con el siguiente formato por ejemplo 192.168.1.1/24: " IP_ADDRESS
 
 		sudo hostnamectl set-hostname "$NEW_HOSTNAME"
-
-		sudo mv /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.$(date +%Y%m%d%T)
-
-		sudo cat > 00-installer-config.yaml << "EOF"
-network:
-  ethernets:
-    ens33:
-      dhcp4: false
-      addresses:"
-       - [$IP_ADDRESS]
-      routes:"
-       - to: default"
-         via: $GATEWAY_ADDRESS
-      nameservers:
-        addresses: [$PRIMARY_DNS_ADDRESS]
-  version: 2
-EOF
-		sudo mv 00-installer-config.yaml /etc/netplan/00-installer-config.yaml
-		sudo netplan apply
+  		sudo ifconfig ens33 $IP_ADDRESS
 	fi
 fi
